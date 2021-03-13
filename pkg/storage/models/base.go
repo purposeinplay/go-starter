2951b29dba7ffbb7fec14c816fdb7f500e875411
jorgepurposeinplay <jorge.molero@purposeinplay.com>
2021-03-13 10:54:53 +0100

    Replace base model DeletedAt with soft delete

--- pkg/storage/models/base.go
+++ pkg/storage/models/base.go
@@ -1,13 +1,14 @@
 package models
 
 import (
-"github.com/pborman/uuid"
-"time"
+	"github.com/pborman/uuid"
+	"gorm.io/gorm"
+	"time"
 )
 
 type Base struct {
-	ID        uuid.UUID     `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"update_at"`
-	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+	ID        uuid.UUID      `json:"id" gorm:"type:uuid, primaryKey, default:uuid_generate_v4()"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"update_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
