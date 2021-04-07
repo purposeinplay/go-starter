18eca6d68c7121264b422793724dd6456ba332ba
jorgepurposeinplay <jorge.molero@purposeinplay.com>
2021-04-07 18:23:34 +0200

    Replace logrus with zap in cmd/migrate.go

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -3,9 +3,8 @@ package cmd
 import (
 	"github.com/oakeshq/go-starter/config"
 	"github.com/oakeshq/go-starter/pkg/storage/dialer"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+	"go.uber.org/zap"
 )
 
 var migrateCmd = &cobra.Command{
@@ -13,23 +12,17 @@ var migrateCmd = &cobra.Command{
 	Long: "Migrate database strucutures. This will create new tables and add missing columns and indexes.",
 	Run: func(cmd *cobra.Command, args []string) {
 		c, err := config.LoadConfig(cmd)
-
 		if err != nil {
-			logrus.Fatalf("Unable to read config %v", err)
+			zap.L().Sugar().Fatalf("Unable to read config %v", err)
 		}
 
-		logrus.SetFormatter(&prefixed.TextFormatter{
-			ForceFormatting: true,
-		})
-
 		db, err := dialer.Connect(c)
-
 		if err != nil {
-			logrus.Fatalf("Error opening database: %+v", err)
+			zap.L().Sugar().Fatalf("Error opening database: %+v", err)
 		}
 
 		if err = dialer.Migrate(db); err != nil {
-			logrus.Fatal(err)
+			zap.L().Sugar().Fatalf("Error migrating database:", err)
 		}
 	},
 }
