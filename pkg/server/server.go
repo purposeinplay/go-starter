cec78e6d4ab7348726e62677e9838fd8a661ea8d
jorgepurposeinplay <jorge.molero@purposeinplay.com>
2021-04-07 18:27:12 +0200

    Replace logrus with zap in pkg/server/server.go

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,14 +2,16 @@ package server
 
 import (
 	"fmt"
-	config2 "github.com/oakeshq/go-starter/config"
-	"github.com/sirupsen/logrus"
 	"net/http"
+
+	config2 "github.com/oakeshq/go-starter/config"
+	"go.uber.org/zap"
 )
 
 // ListenAndServe starts the REST API.
 func ListenAndServe(cfg *config2.Config, h http.Handler) error {
 	location := fmt.Sprintf("%v:%v", cfg.SERVER.Host, cfg.SERVER.Port)
-	logrus.Infof("API started on: %s", location)
+	zap.L().Sugar().Infof("API started on: %s", location)
 	return http.ListenAndServe(location, h)
 }
+
