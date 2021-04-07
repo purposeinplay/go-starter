81521de62230c998a84b72edc74d4c9550d3160c
jorgepurposeinplay <jorge.molero@purposeinplay.com>
2021-04-07 18:25:07 +0200

    Replace logrus with zap in cmd/http.go

--- cmd/http.go
+++ cmd/http.go
@@ -7,31 +7,23 @@ import (
 	"github.com/oakeshq/go-starter/pkg/server"
 	"github.com/oakeshq/go-starter/pkg/storage/dialer"
 
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+	"go.uber.org/zap"
 )
 
 var HTTPCmd = &cobra.Command{
 	Use:   "http",
 	Short: "Starts the http server",
 	Run: func(cmd *cobra.Command, args []string) {
-		logrus.SetFormatter(&prefixed.TextFormatter{
-			ForceFormatting: true,
-		})
-
 		cfg, err := config.LoadConfig(cmd)
-
 		if err != nil {
-			logrus.Fatalf("Unable to read config %v", err)
+			zap.L().Sugar().Fatalf("Unable to read config %v", err)
 		}
 
 		db, err := dialer.Connect(cfg)
-
 		if err != nil {
-			logrus.Fatalf("Error opening database: %+v", err)
+			zap.L().Sugar().Fatalf("Error opening database: %+v", err)
 		}
-
 
 		r := router.NewRouter()
 		api.NewAPI(cfg, r, db)
